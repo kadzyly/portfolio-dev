8315cc8553ef6d8b85c2a20d0213fb897c27a8da
KadzyLy <kadzyly@gmail.com>
2025-11-30 22:12:54 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://kadzyly.github.io",
-  base: import.meta.env.MODE === "production" ? "/portfolio-dev" : "/" ,
+  site: "https://kadzyly.github.io/portfolio-dev/",
+  base: import.meta.env.MODE === "production" ? "/portfolio-dev/" : "/",
   integrations: [react(), mdx()],
   vite: {
-    plugins: [
-      tailwindcss()
-    ],
+    plugins: [tailwindcss()],
     resolve: {
       alias: {
-        '@/': new URL('./src/', import.meta.url).pathname
+        "@/": new URL("./src/", import.meta.url).pathname,
       },
     },
   },
