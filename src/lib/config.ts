a40000e93bccea4cd71187c0c1619b046e155c11
KadzyLy <kadzyly@gmail.com>
2025-11-30 20:03:38 +0100

    feat: add og image

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,6 +2,7 @@ export const SITE = {
   pageType: "website",
   author: import.meta.env.PUBLIC_PROJECT_AUTHOR,
   profile: `https://github.com/${import.meta.env.PUBLIC_USERNAME}`,
+  ogImage: '/og-image.png',
   desc: "Portfolio",
   title: import.meta.env.PUBLIC_PROJECT_AUTHOR,
   postPerPage: 10,
